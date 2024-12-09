c67743c012829d672f91e0cf5f15874b25f4f256
KalleH92 <kalle.hagberg92@hotmail.com>
2024-12-09 11:24:44 +0100

    first commit

---
+++ src/main/java/com/kalleH/exGamne/ExGamneApplication.java
@@ -0,0 +1,13 @@
+package com.kalleH.exGamne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExGamneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExGamneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalleH/exGamne/ExGamneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalleH.exGamne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExGamneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
